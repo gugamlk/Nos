5c8078fd41ed014b5d1f2e7a812ef04c1cdc1a80
gugamlk <91146605+gugamlk@users.noreply.github.com>
2021-11-10 10:00:16 -0300

    Login, começo

---
+++ app/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../estily/boots1.css">
+    <link rel="stylesheet" href="../estily/boots.java">
+    <link rel="stylesheet" href="../estily/boots1.java">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        if(isset($_GET['home'])){
+
+        }else if(isset($_GET['cadastro'])){
+            
+        }else if(isset($_GET['perfil'])){
+            
+        }else if(isset($_GET['mensagens'])){
+            
+        }else if(isset($_GET['salvos'])){
+            
+        }else if(isset($_GET['sobre'])){
+            
+        }else{
+            include_once '../frontend/login.php';
+        }
+    ?>
+</body>
+</html>
--- frontend/login.php
+++ frontend/login.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    <div class="container container-fluid text-center">
-        <form action="../backend/login.php" method="post">
-            <label for="nick">Digite seu apelido</label>
-            <br>
-            <input type="text" name="nick_user">
-            <br>
-            <label for="senha">Digite sua senha</label>
-            <br>
-            <input type="password" name="senha_user">
-            <br>
-            <input type="submit" name="entrar" value="Entrar">
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container container-fluid bg-primary text-center">
+        <form action="login.php">
+            <div class="inp">
+                <label for="Nick">
+                    <h3> Digite seu Nick: </h3>
+                </label>
+                <br>
+                <input type="text" name="nick" id="Nick">
+                <br>
+            </div>
+            <div class="inp">
+                <label for="senha">
+                    <h3> Digite sua Senha:</h3>
+                </label>
+                <br>
+                <input type="password" name="senha" id="senha">
+            </div>
         </form>
     </div>
-    Não é cadastrado ainda?
-    <br>
-    <a href="../frontend/cadastro.php">Clique aqui para se cadastrar</a>
-    </body>
+</body>
+
 </html>
