05a941f2d51ca0a75f5cc07aa900e9e9f17aadc5
gugamlk <Gustavo.S3mSono@gmail.com>
2021-11-17 22:14:07 -0300

    Area de login bonita 1

--- frontend/cadastro.php
+++ frontend/cadastro.php
@@ -1,40 +1,60 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="../css/cadastro.css" rel="stylesheet">
-        <link rel="stylesheet" href="../estily/boots1.css">
-        <link rel="stylesheet" href="../estily/boots2.java">
-        <link rel="stylesheet" href="../estily/boots3.java">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
-    </head>
-    <title>Cadastro</title>
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/cadastro.css" rel="stylesheet">
+    <link rel="stylesheet" href="../estily/boots1.css">
+    <link rel="stylesheet" href="../estily/boots2.java">
+    <link rel="stylesheet" href="../estily/boots3.java">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<div class="container">
+<title>Cadastro</title>
+</head>
+
+<style>
+
+</style>
+
+<body>
+    <div class="box">
+        <fieldset>
+            <legend><b>Cadastro de Novos Usuarios</b></legend>
+        </fieldset>
+
         <form action="../backend/cadastro.php" method="post">
-        <label for="Digite seu nome completo">Digite seu nome completo</label> 
-        <br>
-        <input type="text" name="nome_user">
-        <br>
-        <label for="nick_user">Digite seu apelido</label>
-        <br>
-        <input type="text" name="nick_user">
-        <br>
-        <label>Digite seu email:</Label>
-        <br>
-        <input type="email" name="email_user">
-        <br>
-        <label for="senha">Digite sua senha</label>
-        <br>
-        <input type="password" name="senha_user">
-        <br>
-        <input type="submit" name="cadastro" value="Criar cadastro">
-        <br>
+            <div class="inputbox">
+                <label for="nome_completo">Digite seu nome completo</label>
+                <br>
+                <input type="text" name="nome_user" id="nome_completo">
+            </div>
+            <br>
+            <div class="inputbox">
+                <label for="nick_user">Digite seu apelido</label>
+                <br>
+                <input type="text" name="nick_user" id="nick_user">
+            </div>
+            <br>
+            <div class="inputbox">
+                <label for="email">Digite seu email:</Label>
+                <br>
+                <input type="email" name="email_user" id="email">
+            </div>
+            <br>
+            <div class="inputbox">
+                <label for="senha">Digite sua senha</label>
+                <br>
+                <input type="password" name="senha_user" id="senha">
+            </div>
+            <br>
+            <input type="submit" name="cadastro" value="Criar cadastro">
+            <br>
         </form>
         <form action="../frontend/login.php" method="post">
-        <input type="submit" name="login" value="Login">
-        <br>
+            <input type="submit" name="login" value="Login">
+            <br>
     </div>
+</body>
--- frontend/login.php
+++ frontend/login.php
@@ -10,27 +10,21 @@
     <link rel="stylesheet" href="../estily/boots2.java">
     <link rel="stylesheet" href="../estily/boots3.java">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../estily/loginS.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<style>
-    div{
-        background-color: blue; 
-        top: 50%; 
-    }
-</style>
 <body>
-    <div class="container container-fluid text-center" id="login">
+    <div>
         <h1>Login</h1>
         <form action="../backend/login.php" method="post">
             <input type="text" name="nick_user" placeholder="Nickname">
-            <br>
+            <br><br>
             <input type="password" name="senha_user" placeholder="Senha">
-            <br>
-            <input type="submit" name="entrar" value="Entrar">
+            <br><br>
+            <button type="submit" name="entrar">Entrar</button>
         </form>
-        Não é cadastrado ainda?
         <br>
-        <a href="../frontend/cadastro.php">Clique aqui para se cadastrar</a>
+        <a href="../frontend/cadastro.php"> <button> Clique aqui para se cadastrar </button></a>
     </div>
 </body>
 
