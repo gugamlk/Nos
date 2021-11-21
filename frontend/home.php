1b8fc7a63e5f7953d8559f01831fcf9cc3838bdc
gugamlk <Gustavo.S3mSono@gmail.com>
2021-11-21 20:52:49 -0300

    cabeçalho feito

--- frontend/home.php
+++ frontend/home.php
@@ -13,42 +13,45 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <style>
- .card{
-   border: none;
- }
- #card1{
-   border-right: 2px solid black;
-   border-radius: 0px;
- }
- #card2{
-   border-left: 2px solid black;
- }
+  .card {
+    border: none;
+  }
+
+
+  #card2 {
+    border-left: 2px solid black;
+  }
 </style>
+
 <body>
-    <div class="container container-fluid">
-        <div class="row">
-          <div class="col">
-            <div class="card" id="card1">
-              <div class="card-body">
-                <?php include_once 'main/cHome.php'?>
-              </div>
-            </div>
+  <div class="container container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="card" id="card1">
+          <div class="card-body">
+            <?php include_once 'main/cHome.php'?>
           </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <?php include_once 'main/mHome.php' ?>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body" id="card2">
-                <?php include_once 'main/iHome.php'?>
-              </div>
+        </div>
+      </div>
+      <div class="row">
+
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <?php include_once 'main/mHome.php' ?>
             </div>
           </div>
         </div>
+        <div class="col">
+          <div class="card">
+            <div class="card-body" id="card2">
+              <?php include_once 'main/iHome.php'?>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </body>
+
 </html>
