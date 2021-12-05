d6208c9a0b086664031a7a7e7c1790150e98329e
Gustavo <gustavo.s3msono>
2021-12-05 14:21:39 -0300

    salvando para entrar na branch

--- backend/main.php
+++ backend/main.php
@@ -38,60 +38,81 @@
 
     <?php
 
-        include_once '../classes/conexao.php'; 
-    
+    include_once '../classes/conexao.php';
 
-        #----------------------------------------------------------------------------------------
-        #Selecionar o Banco de Dados
 
-        $selecao = "SELECT * FROM postagens"; 
+    #----------------------------------------------------------------------------------------
+    #Selecionar o Banco de Dados
 
-        $selecao_feita = mysqli_query($conn, $selecao);
+    $selecao = "SELECT * FROM postagens";
+
+    $selecao_feita = mysqli_query($conn, $selecao);
 
     ?>
 
-     <style>
-    
-        .col-sm-4{
+    <style>
+        .col-sm-4 {
             position: relative;
             border: 1px solid black;
             padding: 12px;
         }
-        img{
+
+        img {
             width: 250px;
             margin-left: 70px;
         }
-        p{
+
+        p {
             text-align: center;
             font-family: Arial, Helvetica, sans-serif;
             font-size: 14px;
         }
-     </style>
-     <div class="conatiner container-fluid">
-    <?php while($conjunto = mysqli_fetch_array($selecao_feita)){ ?>
+        button{
+            border: none;
+            position: absolute;
+            background-image: none;
+            background-color: white;
+        }
+    </style>
+    <div class="conatiner container-fluid">
+        <?php while ($conjunto = mysqli_fetch_array($selecao_feita)) {
+
+            #----------------------------------------------------
+            #Sistema de Salvamento (Pegando as Informações do user logado)
+
+            $id_user = $_SESSION['info_logado'];
+
+        ?>
         <div class="row">
             <div class="col-sm-4">
                 <div class="card">
                     <div class="card-heard">
-                        <h4><?php echo$conjunto[1];?></h4>
+                        <h4><?php echo $conjunto[1]; ?></h4>
                     </div>
                     <div class="card-body">
                         <img src="<?php echo $conjunto[3]; ?>" alt="não foi">
                         <br><br>
                         <p><?php echo $conjunto[4]; ?></p>
+                        <br><br>
+                        <a href="../frontend/home.php?<?php echo $id_user[0]; ?>">
+                            <button>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                                    class="bi bi-bookmark-heart-fill" viewBox="0 0 16 16">
+                                    <path d="M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v13.5zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z" />
+                                </svg>
+                            </button>
+                        </a>
+                        <br><br>
                     </div>
                     <div class="card-footer text-center">
                         Publicado Por: <?php echo $conjunto[2]; ?>
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
         <br><br>
-        <?php 
-            if(isset($_SESSION['']))
-        ?>
-        <?php }?>
-     </div>
+        <?php } ?>
+    </div>
 </body>
 
 </html>
