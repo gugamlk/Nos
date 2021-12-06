3f5b61da7a975efcddf2ae1eae7e0cfd06d927a5
morffeu <91982448+morffeu@users.noreply.github.com>
2021-12-05 21:36:04 -0300

    bulas

--- frontend/bulas.php
+++ frontend/bulas.php
@@ -27,12 +27,16 @@
    <div class="col">
       <div class="card">
 
-      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+      <img class="img card-img-top" src="../img/alprazolam.jpg" alt="Imagem do remédio">
 
       <p class="remedionome card-text">Alprazolam</p>
       <p class="remedioresumo card-text">É um remédio indicado para tratamento de transtornos de ansiedade e pânico, age entre 30 minutos ou até 2 horas, e alguns dos possíveis efeitos colaterais são sono, sedação, boca seca e irritabilidade.</p>  
 
-      <a href="../img/bula1.pdf" download>PDF da Bula</a>
+      <a href="../img/alprazolambula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
 
       </div>
     </div>
@@ -40,12 +44,15 @@
     <div class="col">
       <div class="card">
 
-      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+      <img class="img card-img-top" src="../img/citalopram.jpg" alt="Imagem do remédio">
 
-      <p class="remedionome card-text">Alprazolam</p>
-      <p class="remedioresumo card-text">É um remédio indicado para tratamento de transtornos de ansiedade e pânico, age entre 30 minutos ou até 2 horas, e alguns dos possíveis efeitos colaterais são sono, sedação, boca seca e irritabilidade.</p>  
+      <p class="remedionome card-text">Citalopram</p>
+      <p class="remedioresumo card-text">É um medicamento de uso adulto para tratamento e prevenção da depressão e transtornos de ansiedade. Os efeitos podem ser sentidos entre 2 à 4 semanas, podendo trazer reações adversas como a insônia ou sonolência, boca seca, náuseas e a diminuição do apetite.</p>  
 
-      <a href="../img/bula1.pdf" download>PDF da Bula</a>
+      <a href="../img/citaloprambula.pdf" download> 
+        <button class="button">
+      PDF da Bula
+      </button></a>
 
       </div>
 </div>
@@ -53,12 +60,16 @@
     <div class="col">
       <div class="card">
 
-      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+      <img class="img card-img-top" src="../img/escitalopram.jpg" alt="Imagem do remédio">
 
-      <p class="remedionome card-text">Alprazolam</p>
-      <p class="remedioresumo card-text">É um remédio indicado para tratamento de transtornos de ansiedade e pânico, age entre 30 minutos ou até 2 horas, e alguns dos possíveis efeitos colaterais são sono, sedação, boca seca e irritabilidade.</p>  
+      <p class="remedionome card-text">Escitalopram</p>
+      <p class="remedioresumo card-text">Indicado para o tratamento de pacientes que sofram com depressão, síndrome do pânico, TAG, fobia social e TOC. Age em 2 semanas e os principais efeitos colaterais são náuseas e dores de cabeça.</p>  
 
-      <a href="../img/bula1.pdf" download>PDF da Bula</a>
+      <a href="../img/escitaloprambula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
 
       </div>
 </div>
@@ -70,12 +81,87 @@
    <div class="col">
       <div class="card">
 
+      <img class="img card-img-top" src="../img/fluoxetina.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Fluoxetina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+      <a href="../img/fluxetinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+      
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/mirtazapina.jpeg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Mirtazapina</p>
+      <p class="remedioresumo card-text">Trata a depressão. Algumas das reações são sonolência, sedação, cefaleia, boca seca, aumento do peso e aumento do apetite.</p>  
+
+      <a href="../img/mirtazapinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+</div>
+
+    <div class="col">
+      <div class="card">
+
       <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
 
-      <p class="remedionome card-text">Alprazolam</p>
-      <p class="remedioresumo card-text">É um remédio indicado para tratamento de transtornos de ansiedade e pânico, age entre 30 minutos ou até 2 horas, e alguns dos possíveis efeitos colaterais são sono, sedação, boca seca e irritabilidade.</p>  
+      <p class="remedionome card-text">O ultimo</p>
+      <p class="remedioresumo card-text">analise</p>  
 
-      <a href="../img/bula1.pdf" download>PDF da Bula</a>
+      <a href="../img/bula1.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+</div>
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<div class="row">
+
+   <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Agomelatina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/agomelatinabula.pdf" download> 
+      <button class="button">
+      PDF da Bula
+      </button></a>
 
       </div>
     </div>
@@ -85,28 +171,284 @@
 
       <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
 
-      <p class="remedionome card-text">Alprazolam</p>
-      <p class="remedioresumo card-text">É um remédio indicado para tratamento de transtornos de ansiedade e pânico, age entre 30 minutos ou até 2 horas, e alguns dos possíveis efeitos colaterais são sono, sedação, boca seca e irritabilidade.</p>  
+      <p class="remedionome card-text">Bupropiona</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
 
-      <a href="../img/bula1.pdf" download>PDF da Bula</a>
+      <a href="../img/bupropionabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
 
       </div>
-</div>
+    </div>
 
     <div class="col">
       <div class="card">
 
       <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
 
-      <p class="remedionome card-text">Alprazolam</p>
-      <p class="remedioresumo card-text">É um remédio indicado para tratamento de transtornos de ansiedade e pânico, age entre 30 minutos ou até 2 horas, e alguns dos possíveis efeitos colaterais são sono, sedação, boca seca e irritabilidade.</p>  
+      <p class="remedionome card-text">Buspirona</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
 
-      <a href="../img/bula1.pdf" download>PDF da Bula</a>
+      <a href="../img/buspironabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
 
       </div>
-</div>
- 
+    </div>
 </div>
 
+<div class="row">
+
+   <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Duloxetina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/duloxetinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Hidroxizina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/hidroxizinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Imipramina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/imipraminabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+</div>
+<div class="row">
+
+   <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Maprotilina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/maprotilinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Ocinaplon</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/ocinaplonbulabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Opipramol</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/opipramolbula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+</div>
+<div class="row">
+
+   <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Paroxetina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/paroxetinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Pregabalina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/pregabalinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Quetiapina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/quetiapinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+</div>
+<div class="row">
+
+   <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Sertralina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/sertralinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+    </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Tiagabina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/tiagabinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Venlafaxina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/venlafaxinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+</div>
+<div class="row">
+
+   <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Vilazodona</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/vilazodonabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+
+      <img class="img card-img-top" src="../img/testebulas.jpg" alt="Imagem do remédio">
+
+      <p class="remedionome card-text">Vortioxetina</p>
+      <p class="remedioresumo card-text">Utilizado para tratar depressão, ansiedade, bulimia, TOC, sindrome do pânico, transtornos menstruais e disforia. Age em 6 ou 8 horas, mas para sentir efeitos terapêuticos evidentes é necessário a passagem do período de 2~6 semanas</p>  
+
+      <a href="../img/vortioxetinabula.pdf" download>
+      <button class="button">
+      PDF da Bula
+      </button>
+      </a>
+
+      </div>
+    </div>
+
+</div>
 </body>
 </html>
